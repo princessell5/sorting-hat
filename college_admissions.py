52073a003981b5f7644e54f12c49af882f82dfc3
stem league <64894084+stemleague@users.noreply.github.com>
2020-05-23 23:57:19 -0700

    Create college_admissions.py

---
+++ college_admissions.py
@@ -0,0 +1,39 @@
+student1 = ['emma', '3.4', 'san jose', 'CA', 'biology']
+student2 = ['derek', '3.3', 'Fremont', 'wa', 'communications']
+student3 = ['tyler', 3.9, 'chapel Hill', 'nC', 'computer science']
+student4 = ['liz', 4, 'new york City', 'ny', 'computer engineering']
+student5 = ['marie', 3.1, 'charlottesville', 'vA', 'civil engineering']
+
+#### PART 1 ####
+# Add all five students to a list called all_students
+
+##### YOUR CODE HERE #####
+
+
+
+
+
+
+
+
+# Now the UW Seattle wants to process all of the students state locations, but ignoring case. Write a series of print statements that will print each applicant's City and State. For example, for Emma we want to print 'San Jose, CA' with S and J and the state name being capitalized.
+
+##### YOUR CODE HERE #####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
