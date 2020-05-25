f2a2ac89568708e46b719b03090e3bf75c27a54e
stem league <64894084+stemleague@users.noreply.github.com>
2020-05-25 12:52:46 -0700

    Update college_admissions.py

--- college_admissions.py
+++ college_admissions.py
@@ -19,6 +19,7 @@ derek_classes = {'AP Physics', 'Teaching Academy', 'Creative Writing'}
 tyler_classes = {'AP Computer Science', 'Nanotechnology', 'Cisco'}
 karina_classes = {'Biology', 'AP Art History'}
 lucas_classes = {'AP Computer Science', 'AP Psychology'}
+
 #### DO NOT CHANGE CODE ABOVE ####
 
 
@@ -45,7 +46,7 @@ lucas_classes = {'AP Computer Science', 'AP Psychology'}
 
 
 
-
+#### PART 2 ####
 # Now the UW Seattle wants to process all of the students locations. Edit each student's city & state from their own list (for ex. Emma's list is emma) & save them to their own variable (for example: Emma's city & state can be in the variable emma_city & emma_state). 
 # To process all of the student's locations, we want to for example, print 'San Jose, CA' with S and J and the state name acronym being capitalized for Emma's location. Essentially for the city, we want the first letter capitalized and the rest would be lowercase. For the the acronym of the state, we want all letters to be capitalized. 
 # NOTE: We can get the upper case version of a string using the function string.upper() and lower case using string.lower(). See the example below:
@@ -80,6 +81,7 @@ print('reformatted : ' + capitalized_city + captialized_city_rest + ', ' + state
 
 
 
+#### PART 3 ####
 # The University is complaining that the system that they used to retrieve information was broken and even the students' names are unformatted. They want to clean the data, and they are asking for your help. Reformat each list to have the first letter in the name & city capitalized (everything else lowercase), both letters in the state to be upper case.You can use your previous city & state variables to make this part easier!
 # Ex: 
 # Before modifying list student1 = ['emma', 3.4, 'saN Jose', 'CA', 'biology']
@@ -105,7 +107,7 @@ print('reformatted : ' + capitalized_city + captialized_city_rest + ', ' + state
 
 
 
-#### PART 2 ####
+#### PART 4 ####
 # Let's try modifying the classes of each student's schedules contained in sets:
 # 1) Add 'AP Biology', 'AP American Literature', & 'Racquet Sports' to Emma's classes
 # 2) Remove 'Teaching Academy' from Derek's classes & add 'Basketball' to Derek's classes
@@ -133,7 +135,7 @@ print('reformatted : ' + capitalized_city + captialized_city_rest + ', ' + state
 
 
 
-#### PART 3 ####
+#### PART 5 ####
 # Now that we have cleaned up our lists, we can print the output for the University. But UW Seattle is strictly not admitting students who are interested in Computer Science, because they have limited capacity, and also students with lower than a 3.0 GPA. Therefore use conditions to filter out students who don't meet the requirements. The report will be a series of print statements that state:
 
 # Accepted: StudentName from City, State is interested in studying MajorName with a GPA of higher than or equal to a 3.0, so she is admitted to the University.
@@ -147,7 +149,6 @@ print('reformatted : ' + capitalized_city + captialized_city_rest + ', ' + state
 # Accepted: Emma from San Jose, CA is interested in studying biology with a GPA of higher than or equal to a 3.0, so she is admitted to the University.
 
 ##### YOUR CODE HERE #####
-
 
 
 
