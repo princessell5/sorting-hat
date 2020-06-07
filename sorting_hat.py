7fa566c6f43e2d6b6c94c185ef7c2d9dcdde2938
stem league <64894084+stemleague@users.noreply.github.com>
2020-06-06 20:21:37 -0700

    Update sorting_hat.py

--- sorting_hat.py
+++ sorting_hat.py
@@ -1,11 +1,7 @@
+
+#### PART 1: Introduction ####
 print("=== Sorting Hat Quiz ===")
 print("Welcome to Hogwarts! It's time to find out which house you belong in! For each question, please type the number of the answer you choose.\nAre you ready? Time to put on the Sorting Hat...\n")
-
-# Create a variable to keep track of scores for each result.
-# gryffindor = 0
-# hufflepuff = 0
-# ravenclaw = 0
-# slytherin = 0
 
 # Create a list containing each result of the quiz.
 results = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
@@ -13,9 +9,7 @@ results = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 # Create a list 'tallies' that will keep score of each result. This list should be the same length as the 'results' list. Each entry in 'tallies' will keep score for the result with the same index in 'results'.
 tallies = [0] * len(results)
 
-# Now let's write some questions!
-
-# Q1
+# QUESTION 1
 print("Question 1")
 print("What's your favorite element?")
 print("1: Fire")
@@ -29,27 +23,66 @@ answer = int(input("Answer: "))
 # Add a point in 'tallies' for the result that corresponds to the user's answer. Remember that list indexing starts at 0. 
 tallies[answer - 1] += 1
 
-# We can use conditionals to evaluate which result the user's answer corresponds to. We give that result a point that we will tally later.
-# if answer == 1:
-#   gryffindor += 1
-# elif answer == 2:
-#   hufflepuff += 1
-# elif answer == 3:
-#   ravenclaw += 1
-# elif answer == 4:
-#   slytherin += 1
-# else:
-#   print("Invalid answer.")
-# print()
-
-# Your turn! Create some questions of your own using Q1 as an example!
-# ...
 
 
-# Time to report the result!
+#### PART 2: Add Questions ####
+# Now let's write some questions! Make sure your answer choices are either 1, 2, 3, or 4 so it will be added to your tallies list.
 
+# QUESTION 2
+#### YOUR CODE HERE ###
+
+
+
+
+
+
+
+answer = int(input("Answer: "))
+tallies[answer - 1] += 1
+
+# QUESTION 3
+#### YOUR CODE HERE ###
+
+
+
+
+
+
+
+answer = int(input("Answer: "))
+tallies[answer - 1] += 1
+
+# QUESTION 4
+#### YOUR CODE HERE ###
+
+
+
+
+
+
+
+answer = int(input("Answer: "))
+tallies[answer - 1] += 1
+
+# QUESTION 5
+#### YOUR CODE HERE ###
+
+
+
+
+
+
+
+answer = int(input("Answer: "))
+tallies[answer - 1] += 1
+
+
+
+### PART 3: Report the result ####
 # This line finds the index of the maximum score in 'tallies' (if two scores are tied, it takes the first one in the list). Then it finds the value with the corresponding index in 'results' and stores it in the variable 'result'.
-result = results[tallies.index(max(tallies))]
+max_number_from_talliest = max(tallies)
+index_of_max_num = tallies.index(max_number_from_talliest)
+result = results[index_of_max_num]
 
 print("\nAfter a long pause, the Sorting Hat cries:")
 print(result + "!")
